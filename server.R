38f0df3f52544f2a7d8d9be69522678dc8f78535
lincolnharmer <lincoln.harmer@concept3d.com>
2014-12-04 21:55:05 -0700

    Update server.R

--- server.R
+++ server.R
@@ -13,7 +13,7 @@ shinyServer(function(input, output, session) {
   userdata <- reactive(function(){
      if(is.null(input$bugs)){return()}
 	 drv <- dbDriver("SQLite") 
-con <- dbConnect(drv, dbname = "eplusout.sql") 
+con <- dbConnect(drv, dbname = input$bugs$dataath) 
 
        bugs <- dbGetQuery(con, "select VariableName FROM ReportVariableDataDictionary")
 	  
